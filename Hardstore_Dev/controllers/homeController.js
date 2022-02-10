b579d756e3ef97d7a9390fff770af75ce5c7b3bd
fede0104 <federico.r.wagner@gmail.com>
2022-02-10 11:21:04 -0300

    base de datos json provisoria

--- Hardstore_Dev/controllers/homeController.js
+++ Hardstore_Dev/controllers/homeController.js
@@ -1,120 +1,14 @@
-//LO MAS VENDIDO
-mostSold = [
-    {
-        'prod_id': 1,
-        'prod_name': "Monitor Curvo Led 27 Samsung F390",
-        'prod_category': 'monitor',
-        'prod_img':"images/monitor.webp",
-        'price':"$36.000",
-        'price_dto':"$28.800",
-        'dto':"20%OFF",
-    },
-    {
-        'prod_id': 2,
-        'prod_name': "Notebook DELL i5",
-        'prod_category': 'notebook',
-        'prod_img':"images/note.png",
-        'price':"$35.000",
-        'price_dto':"$27.800",
-        'dto':"15%OFF",
-    },
-    {
-        'prod_id': 3,
-        'prod_name': "Auriculares",
-        'prod_category': 'auriculares',
-        'prod_img':"images/auris.png",
-        'price':"$26.000",
-        'price_dto':"$18.800",
-        'dto':"10%OFF",
-    },
-    {
-        'prod_id': 4,
-        'prod_name': "Silla Gamer ROJA",
-        'prod_category': 'silla',
-        'prod_img':"images/silla.jpg",
-        'price':"$10.000",
-        'price_dto':"$8.800",
-        'dto':"5%OFF",
-    }
-]
-//NUESTRA SELECCION
-ourSelection =[
-    {
-        'prod_id': 5,
-        'prod_name': "Monitor Curvo Led 27 Samsung F390",
-        'prod_category': 'monitor',
-        'prod_img':"images/monitor.webp",
-        'price':"$36.000",
-        'price_dto':"$28.800",
-        'dto':"20%OFF",
-    },
-    {
-        'prod_id': 6,
-        'prod_name': "Notebook DELL i5",
-        'prod_category': 'notebook',
-        'prod_img':"images/note.png",
-        'price':"$35.000",
-        'price_dto':"$27.800",
-        'dto':"15%OFF",
-    },
-    {
-        'prod_id': 7,
-        'prod_name': "Auriculares",
-        'prod_category': 'auriculares',
-        'prod_img':"images/auris.png",
-        'price':"$26.000",
-        'price_dto':"$18.800",
-        'dto':"10%OFF",
-    },
-    {
-        'prod_id': 8,
-        'prod_name': "Silla Gamer ROJA",
-        'prod_category': 'silla',
-        'prod_img':"images/silla.jpg",
-        'price':"$10.000",
-        'price_dto':"$8.800",
-        'dto':"5%OFF",
-    }
-]
-//OFERTAS
-offers =[
-    {
-        'prod_id': 9,
-        'prod_name': "Monitor Curvo Led 27 Samsung F390",
-        'prod_category': 'monitor',
-        'prod_img':"images/monitor.webp",
-        'price':"$36.000",
-        'price_dto':"$28.800",
-        'dto':"20%OFF",
-    },
-    {
-        'prod_id': 10,
-        'prod_name': "Notebook DELL i5",
-        'prod_category': 'notebook',
-        'prod_img':"images/note.png",
-        'price':"$35.000",
-        'price_dto':"$27.800",
-        'dto':"15%OFF",
-    },
-    {
-        'prod_id': 11,
-        'prod_name': "Auriculares",
-        'prod_category': 'auriculares',
-        'prod_img':"images/auris.png",
-        'price':"$26.000",
-        'price_dto':"$18.800",
-        'dto':"10%OFF",
-    },
-    {
-        'prod_id': 12,
-        'prod_name': "Silla Gamer ROJA",
-        'prod_category': 'silla',
-        'prod_img':"images/silla.jpg",
-        'price':"$10.000",
-        'price_dto':"$8.800",
-        'dto':"5%OFF",
-    }
-]
+const fs = require('fs');
+const path = require('path')
+
+//Base de Datos de productos
+dataBasePath = path.join(__dirname, '../data_base/productos.json')
+data_base = fs.readFileSync(dataBasePath)
+data_base = JSON.parse(data_base)["data_base"]
+//filtros de productos (secciones de home)
+mostSold = data_base.filter(producto => producto.most_sold == 'True')
+ourSelection = data_base.filter(producto => producto.selection == 'True')
+offers = data_base.filter(producto => producto.offer == 'True')
 
 const controller = {
     home: function(req, res){
--- Hardstore_Dev/controllers/productsController.js
+++ Hardstore_Dev/controllers/productsController.js
@@ -1,42 +1,11 @@
+const fs = require('fs');
+const path = require('path')
 
-data_base =[
-    {
-        'prod_id': 5,
-        'prod_name': "Monitor Curvo Led 27 Samsung F390",
-        'prod_category': 'monitores',
-        'prod_img':"/images/monitor.webp",
-        'price':"$36.000",
-        'price_dto':"$28.800",
-        'dto':"20%OFF",
-    },
-    {
-        'prod_id': 6,
-        'prod_name': "Notebook DELL i5",
-        'prod_category': 'notebook',
-        'prod_img':"/images/note.png",
-        'price':"$35.000",
-        'price_dto':"$27.800",
-        'dto':"15%OFF",
-    },
-    {
-        'prod_id': 7,
-        'prod_name': "Auriculares",
-        'prod_category': 'headsets',
-        'prod_img':"/images/auris.png",
-        'price':"$26.000",
-        'price_dto':"$18.800",
-        'dto':"10%OFF",
-    },
-    {
-        'prod_id': 8,
-        'prod_name': "Silla Gamer ROJA",
-        'prod_category': 'sillas',
-        'prod_img':"/images/silla.jpg",
-        'price':"$10.000",
-        'price_dto':"$8.800",
-        'dto':"5%OFF",
-    }
-]
+
+//Base de Datos de productos
+dataBasePath = path.join(__dirname, '../data_base/productos.json')
+data_base = fs.readFileSync(dataBasePath)
+data_base = JSON.parse(data_base)["data_base"]
 
 const controller = {
     galery: function(req, res){
@@ -53,7 +22,6 @@ const controller = {
         }else{
             results = data_base
         }
-
         res.render("products_galery",
         {
             'results':results
