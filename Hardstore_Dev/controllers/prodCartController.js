9d57624577326a4042371858b98c3c4e4e56b697
gabrielaNieves <gabrielapaez95@gmail.com>
2022-02-15 00:18:56 -0300

    Aplicación vista dinamica carrito

--- Hardstore_Dev/controllers/prodCartController.js
+++ Hardstore_Dev/controllers/prodCartController.js
@@ -6,9 +6,10 @@ dataBasePath = path.join(__dirname, '../data_base/productos.json')
 data_base = fs.readFileSync(dataBasePath)
 data_base = JSON.parse(data_base)["data_base"]
 
+let itemCart = data_base.filter(producto => producto.prod_id <= 7)
 const controller = {
     cart: (req, res) => {
-        res.render("productCart")
+        res.render("productCart", { 'itemCart':itemCart})
     }
 }
 
