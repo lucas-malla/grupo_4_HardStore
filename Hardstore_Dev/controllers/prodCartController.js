8248acf874f2fe59d1de8bb787b9e6a04c81e25e
gabrielaNieves <gabrielapaez95@gmail.com>
2022-02-14 21:25:43 -0300

    Actualizacion ruta carrito

--- Hardstore_Dev/controllers/prodCartController.js
+++ Hardstore_Dev/controllers/prodCartController.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const path = require('path')
 
+//Base de Datos de productos
+dataBasePath = path.join(__dirname, '../data_base/productos.json')
+data_base = fs.readFileSync(dataBasePath)
+data_base = JSON.parse(data_base)["data_base"]
+
 const controller = {
     cart: (req, res) => {
         res.render("productCart")
