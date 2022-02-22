3a35828e37a00e05144f63a5411e2eaeee52af67
fede0104 <federico.r.wagner@gmail.com>
2022-02-22 10:24:31 -0300

    prod id on links

--- Hardstore_Dev/controllers/prodCartController.js
+++ Hardstore_Dev/controllers/prodCartController.js
@@ -12,5 +12,4 @@ const controller = {
     }
 }
 
-
 module.exports = controller
--- Hardstore_Dev/controllers/productsController.js
+++ Hardstore_Dev/controllers/productsController.js
@@ -1,19 +1,22 @@
 const fs = require('fs');
 const path = require('path')
 
-//Base de Datos de productos
-dataBasePath = path.join(__dirname, '../data_base/productos.json')
-data_base = fs.readFileSync(dataBasePath)
-data_base = JSON.parse(data_base)["data_base"]
+function refreshContent(){
+    //Base de Datos de productos
+    dataBasePath = path.join(__dirname, '../data_base/productos.json')
+    data_base = fs.readFileSync(dataBasePath)
+    data_base = JSON.parse(data_base)
+    return data_base
+}
 
 const controller = {
     galery: function(req, res){
         // req.query.search     es el input del usuario al buscar un producto (GET)
         // req.params.filter    es el parametro pasado por url si se abre una categoria
-
         //Busqueda en  Base de Datos
         let filter = req.query.search || req.params.filter
         let results = []
+        refreshContent()
         if (filter){
             results = data_base.filter(producto => 
                 producto.prod_category == filter
