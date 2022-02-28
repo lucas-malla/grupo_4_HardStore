d190eb2d1a2f38b795f15d84f7008e733c3b4670
gabrielaNieves <gabrielapaez95@gmail.com>
2022-02-28 14:37:31 -0300

    Modificaciones carrito compra

--- Hardstore_Dev/controllers/prodCartController.js
+++ Hardstore_Dev/controllers/prodCartController.js
@@ -7,9 +7,19 @@ data_base = fs.readFileSync(dataBasePath)
 data_base = JSON.parse(data_base)["data_base"]
 
 let itemCart = data_base.filter(producto => producto.prod_id <= 7)
-const controller = {
+let random = function(productos){
+let resultado = [];
+    for(let i = 1; i <= 3; i++ ){
+    let aleatorio = productos[Math.floor(Math.random() * productos.length)]
+    resultado.push(aleatorio)
+}
+return resultado
+}
+let showRandom = random(data_base);
+
+const controller = { 
     cart: (req, res) => {
-        res.render("productCart", { 'itemCart':itemCart})
+        res.render("productCart", { 'itemCart':itemCart, showRandom: showRandom})
     }
 }
 
