21f256ac5303c29c7fed79229811e05d3878d7dd
lucas-malla <lucasarien.9210@gmail.com>
2022-05-25 11:01:23 -0300

    validation front-end

--- Hardstore_Dev/public/js/adminProdModification.js
+++ Hardstore_Dev/public/js/adminProdModification.js
@@ -1,131 +1,113 @@
 window.onload = function(){
-    let form = document.querySelector("#formModification");
-    let name = document.querySelector("#inputName");
-    let brand = document.querySelector("#inputBrand");
-    let model = document.querySelector("#inputModel");
-    let color = document.querySelector("#prodColor");
-    let price = document.querySelector("#inputPrice");
-    let dto = document.querySelector("#inputDto");
-    let stock = document.querySelector('#inputStock');
-    let categori = document.querySelector('#inputCategori')
-    let file = document.querySelector('#formFile');
-    let descrip =document.querySelector('#inputDescri')
+     
+            let form = document.querySelector("#formModification");
+            let name = document.querySelector("#inputName");
+            let brand = document.querySelector("#inputBrand");
+            let model = document.querySelector("#inputModel");
+            let color = document.querySelector("#prodColor");
+            let price = document.querySelector("#inputPrice");
+            let dto = document.querySelector("#inputDto");
+            let stock = document.querySelector('#inputStock');
+            let descrip = document.querySelector('#inputDescrip')
+            let file = document.querySelector('#formFile');
 
-    let setError = (element, message) =>{
-        let inputControl = element.parentElement;
-        let errorDisplay = inputControl.querySelector('.error')
-        errorDisplay.innerText = message
-        inputControl.classList.add('error');
-        inputControl.classList.remove('success')
-    };
 
-    let setSuccess = element => {
-        let inputControl = element.parentElement;
-        let errorDisplay = inputControl.querySelector('.error')
-        errorDisplay.innerText = '';
-        inputControl.classList.add('success');
-        inputControl.classList.remove('error')
-    };
-   
-    form.addEventListener('submit', e => {
-        const errores = [];
-         
-
-        if(name.value === ''){
-            setError(name, 'Ingrese un nombre de producto')
-            errores.push('error')
-        }
-        else if(name.value.length < 5){
-            setError(name, 'El nombre debe tener al menos 5 caracteres')
-            errores.push('error')
-        }
-        else{
-            setSuccess(name)
-        }
-
-        if(categori.value === ''){
-            setError(categori, 'Seleccione una categoria')
-            errores.push('error')
-        }else{
-            setSuccess(categori)
-        }
+            form.addEventListener('submit', (e) => {
+            let errors = [];
+                
         
-        if(brand.value === ''){
-            setError(brand, 'Ingrese una marca')
-            errores.push('error')
-        }else{
-            setSuccess(brand)
-        }
+        if (name.value === '') {
+                    errors.push('Ingrese un nombre de producto');
+                    name.classList.add('is-invalid');
+                } else if(name.value.length <5) {
+                    errors.push('El nombre debe tener al menos 5 caracteres');  
+                    name.classList.add('is-invalid');  
+                }else{
+                    name.classList.add('is-valid');
+                    name.classList.remove('is-invalid');
+                     form.brand.focus();
+                }
 
-        if(model.value === ''){
-            setError(model, 'Ingrese un modelo')
-            errores.push('error')
-        }else{
-            setSuccess(model)
-        }
+                if (brand.value === '') {
+                    errors.push('Ingrese una marca');
+                    brand.classList.add('is-invalid');
+                } else {
+                    brand.classList.add('is-valid');
+                    brand.classList.remove('is-invalid');
+                    form.model.focus();
+                };
 
-        if(color.value === ''){
-            setError(color, 'Ingrese un color')
-            errores.push('error')
-        }else{
-            setSuccess(color)
-        }
+                if (model.value === '') {
+                    errors.push('Ingrese un modelo');
+                    model.classList.add('is-invalid');
+                } else {
+                    model.classList.add('is-valid');
+                    model.classList.remove('is-invalid');
+                    form.color.focus();
+                };
+                if (color.value === "") {
+                    errors.push('Ingrese un color');
+                    color.classList.add('is-invalid');
+                } else {
+                    color.classList.add('is-valid');
+                    color.classList.remove('is-invalid');
+                    form.price.focus();
+                };
+                
+                 if (price.value === '') {
+                    errors.push('Ingrese un precio');
+                    price.classList.add('is-invalid');
+                } else {
+                    price.classList.add('is-valid');
+                    price.classList.remove('is-invalid');
+                    form.dto.focus();
+                };
 
-        if(price.value === ''){
-            setError(price, 'Ingrese un precio')
-            errores.push('error')
-        }else{
-            setSuccess(price)
-        }
+                if (dto.value === '') {
+                    errors.push('Ingrese un descuento');
+                    dto.classList.add('is-invalid');
+                } else {
+                    dto.classList.add('is-valid');
+                    dto.classList.remove('is-invalid');
+                    form.stock.focus();
+                };
 
-        if(dto.value === ''){
-            setError(desc, 'Ingrese un valor')
-            errores.push('error')
-        }else{
-            setSuccess(dto)
-        }
+                if (stock.value === '') {
+                    errors.push('Ingrese el stock');
+                    stock.classList.add('is-invalid');
+                } else {
+                    stock.classList.add('is-valid');
+                    stock.classList.remove('is-invalid');
+                };
 
-        if(stock.value === ''){
-            setError(stock, 'Ingrese el stock')
-            errores.push('error')
-        }else{
-            setSuccess(stock)
-        }
+                if (descrip.value === '') {
+                    errors.push('Ingrese la descrpcion');
+                    descrip.classList.add('is-invalid');
+                } else {
+                    descrip.classList.add('is-valid');
+                    descrip.classList.remove('is-invalid');
+                };
+               
+                // if (file.value === '') {
+                //     errors.push('Ingrese una imagen');
+                //     file.classList.add('is-invalid');
+                // } else {
+                //     file.classList.add('is-valid');
+                //     file.classList.remove('is-invalid');
+                // }
 
-        if(descrip.value === ''){
-            setError(descrip, 'Ingrese una descripción')
-            errores.push('error')
-        } 
-        else if(descrip.value.length < 20){
-            setError(descrip, 'La decripción debe tener al menos 20 carácteres')
-            errores.push('error')
-        }
-        else{
-            setSuccess(descrip)
-        }
-
-        if(file.value === ''){
-            setError(file, 'Ingrese una imagen')
-            errores.push('error')
-        }
-        else if(file.value != ""){
-            const whitelist = ['png', 'jpeg', 'jpg', 'webp']   
-            if (!whitelist.includes(file.value.split(".")[1])){
-                setError(file, 'La imagen debe ser formato: \n .png , jpeg , jpg o webp')
-                errores.push('error')
-            }
-            else{
-                setSuccess(file)
-            }
-        }
-
-        if (errores.length > 0) {
-            e.preventDefault();
-        }else{
-            form.submit();
-        }
-
-    
-    })
+        if (errors.length > 0) {
+                    e.preventDefault();
+                    let ulErrors = document.querySelector('.errores');
+                    ulErrors.classList.add('alert-warning');
+                    ulErrors.innerHTML = '';
+                    for (let i = 0; i < errors.length; i++) {
+                        ulErrors.innerHTML += `<li>${errors[i]} </li>`;
+                    };
+                }else{
+                    form.submit();
+                }
+        
+            });
 
 }
